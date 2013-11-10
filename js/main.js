a2cc33b8403248dc9182562b6c45cc8a930bf57f
Ricardo Lopez <rlopez4@wisc.edu>
2013-11-09 20:13:25 -0600

    added regexs for statements

--- js/main.js
+++ js/main.js
@@ -1,1 +1,42 @@
+var inputText = '';
 
+$(document).ready(function() {
+
+	$('#submitCode').on('click', function() {
+		inputText = $('#textInputArea').val();
+	});
+
+});
+
+
+var symbols = (function() {
+	var stmts = {
+		fnStmt: "function[ ][a-zA-z]+[ ]?\n",
+
+		ifStmt : "if[ ][a-zA-z]+[ ]?\n",
+
+		elseifStmt: "elseif[ ][a-zA-z]+[ ]?\n",
+
+		elseStmt : "else[ ]?\n",
+
+		whileStmt : "while[ ][a-zA-z]+[ ]?\n", 
+
+		endStmt : "end"
+	};
+
+
+});
+
+
+function blockObject(title, stmtType, stmts, children, posX, posY, color) {
+	this.title = title;
+	this.stmtType = stmtType;
+	this.stmts = stmts;
+	this.children = children;
+	this.color = color;
+
+	this.posX = posX;
+	this.poxY = posY;
+
+
+}
